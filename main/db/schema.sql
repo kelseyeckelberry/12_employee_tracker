bd81d93418c2b780b8ea67541258a6eafc994aed
kelseck <kelseyeckelberry@gmail.com>
2020-12-08 23:43:49 -0600

    functions

--- main/db/schema.sql
+++ main/db/schema.sql
@@ -6,24 +6,34 @@ USE employeeTracker_db;
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY(id)
 );
+
+CREATE UNIQUE INDEX department_name_idx on department(name);
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10, 2) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  department_id INT NOT NULL,
+  PRIMARY KEY(id),
+  INDEX (department_id),
+  FOREIGN KEY(department_id) REFERENCES department(id)
 );
+
+CREATE UNIQUE INDEX role_title_idx on role(title);
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  PRIMARY KEY(id),
+  INDEX (role_id),
+  INDEX (manager_id),
+  FOREIGN KEY(role_id) REFERENCES role(id),
+  FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
 
 
--- main/index.js
+++ main/index.js
@@ -9,5 +9,70 @@ const connection = mysql.createConnection({
     user: "root",
 
     password: "password",
-    database: "employee_tracker_db"
+    database: "employeeTracker_db"
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+  });
+
+function start() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            //"View All Employees", 
+            "Add Employee", 
+            "Add Department",
+            "Add Role",
+            "Update Employee Role",
+            "View Employees", 
+            "View Departments",
+            "View Roles",
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action){
+            case "Add Employee":
+                addData("employee");
+              break;
+            case "Add Department":
+                addData("department");
+              break;
+            case "Add Role":
+                addData("role");
+              break;
+            case "View Employees":
+                viewDB("employee");
+              break;
+            case "View Departments":
+                viewDB("department");
+              break;
+            case "View Roles":
+                viewDB("role");
+              break;
+            case "Update Employee Role":
+                updateData("___");
+              break;
+
+            default:
+              break;
+        }
+    });
+};
+
+function addData(dataName) {
+
+};
+
+function viewDB(dataName) {
+
+};
+
+function updateData(dataName) {
+
+};
+
